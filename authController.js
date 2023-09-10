8be24c39469a649fa64ae55e0a0590d688ea7e55
NickZavadski <nick.mr.volk@gmail.com>
2023-09-10 15:38:19 +0200

    add user

--- authController.js
+++ authController.js
@@ -1,23 +1,39 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
+const bcrypt = require('bcryptjs')
 
 class AuthController {
 	async registration(req, res) {
 		try {
-		} catch (e) {}
+			const { username, password } = req.body
+			const candidate = await User.findOne({ username })
+			if (candidate) {
+				return res.status(400).json({ message: 'User with this login exist' })
+			}
+			const hashPassword = bcrypt.hashSync(password, 7)
+			const userRole = await Role.findOne({ value: 'USER' })
+			const user = new User({ username, password: hashPassword, roles: [userRole.value] })
+			await user.save()
+			return res.json({ message: 'User successfully added' })
+		} catch (e) {
+			console.log(e)
+			res.status(400).json({ message: 'Registration error' })
+		}
 	}
 	async login(req, res) {
 		try {
-		} catch (e) {}
+		} catch (e) {
+			console.log(e)
+			res.status(400).json({ message: 'Login error' })
+		}
 	}
 	async getUsers(req, res) {
-		const userRole = new Role()
-		const adminRole = new Role({ value: 'ADMIN' })
-		await userRole.save()
-		await adminRole.save()
 		try {
 			res.json(`server work`)
-		} catch (e) {}
+		} catch (e) {
+			console.log(e)
+			res.status(400).json({ message: 'Get users error' })
+		}
 	}
 }
 
